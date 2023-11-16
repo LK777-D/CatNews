f7bfa3175ac690b3dbf2e27cc1e5f3dc744087b3
LK777-D <luka.dolbaia17@gmail.com>
2023-11-16 16:42:30 +0400

    blogposts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqj1wdhdvMPLMXNlZBm68L2Jwt2TKiyCw",
-  authDomain: "cat-news-78068.firebaseapp.com",
-  projectId: "cat-news-78068",
-  storageBucket: "cat-news-78068.appspot.com",
-  messagingSenderId: "455336175529",
-  appId: "1:455336175529:web:b1839ed86bdde540d246b8"
+  apiKey: "AIzaSyDjONP0Oyq016Hx_QkEAVO_ipa_6utbq70",
+  authDomain: "cat-news2.firebaseapp.com",
+  projectId: "cat-news2",
+  storageBucket: "cat-news2.appspot.com",
+  messagingSenderId: "651642003705",
+  appId: "1:651642003705:web:45dd68a31c0269436a8228"
 };
 
 // Initialize Firebase
